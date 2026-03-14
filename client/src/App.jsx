795353454ddb4da0b338e94c91690dc6c1a9db40
paarth293 <i.m.paarthgupta@gmail.com>
2026-03-14 18:07:18 +0530

    updated the app.jsc file routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
 import DashboardPage from './pages/Dashboard.jsx';
 import LoginPage from './pages/Login.jsx';
 import LeadsPage from './pages/Leads.jsx';
@@ -15,24 +16,35 @@ import DealsPage from './pages/Deals.jsx';
 import ReportsPage from './pages/Reports.jsx';
 import SettingsPage from './pages/Settings.jsx';
 import CustomerDetailPage from './pages/CustomerDetail.jsx';
+import RegisterUserPage from './pages/RegisterUser.jsx';
+import TeamsPage from './pages/Teams.jsx';
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/login" replace />;
+}
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/leads" element={<LeadsPage />} />
-        <Route path="/tasks" element={<TasksPage />} />
-        <Route path="/executive" element={<ExecutiveDashboardPage />} />
-        <Route path="/customers" element={<CustomersPage />} />
-        <Route path="/customers/:id" element={<CustomerDetailPage />} />
-        <Route path="/deals" element={<DealsPage />} />
-        <Route path="/reports" element={<ReportsPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="*" element={<Navigate to="/dashboard" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+          <Route path="/leads" element={<PrivateRoute><LeadsPage /></PrivateRoute>} />
+          <Route path="/deals" element={<PrivateRoute><DealsPage /></PrivateRoute>} />
+          <Route path="/tasks" element={<PrivateRoute><TasksPage /></PrivateRoute>} />
+          <Route path="/executive" element={<PrivateRoute><ExecutiveDashboardPage /></PrivateRoute>} />
+          <Route path="/customers" element={<PrivateRoute><CustomersPage /></PrivateRoute>} />
+          <Route path="/customers/:id" element={<PrivateRoute><CustomerDetailPage /></PrivateRoute>} />
+          <Route path="/reports" element={<PrivateRoute><ReportsPage /></PrivateRoute>} />
+          <Route path="/settings" element={<PrivateRoute><SettingsPage /></PrivateRoute>} />
+          <Route path="/register-user" element={<PrivateRoute><RegisterUserPage /></PrivateRoute>} />
+          <Route path="/teams" element={<PrivateRoute><TeamsPage /></PrivateRoute>} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
