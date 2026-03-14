12251177407529c7dda176cab8f1097dbbc85b33
paarth293 <i.m.paarthgupta@gmail.com>
2026-03-14 23:13:51 +0530

    fixes

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); // ⚠️ Must be first before any other require
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
